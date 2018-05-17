4dcd4e65bf9f0b4d8f3f570a7a42577ef4060b45
Carlos A. Ospina <carlosaospinac@gmail.com>
2018-05-16 21:11:51 -0500

    Create react app structure

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">AMID UN</h1>
+        </header>
+        <p className="App-intro">
+          Hello world!
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
