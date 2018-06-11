88c43bf2ea201c384154bb8b9b01f3e28913037d
Carlos A. Ospina <carlosaospinac@gmail.com>
2018-06-10 22:59:32 -0500

    Quit semantic-ui libraries

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import 'font-awesome/css/font-awesome.css';
-import 'semantic-ui-css/semantic.min.css';
 import 'primereact/resources/primereact.min.css';
 
 /* THEMES
