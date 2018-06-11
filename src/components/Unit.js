21ee5c8a71c71a616b4006b795133b5896d7e0a1
Carlos A. Ospina <carlosaospinac@gmail.com>
2018-06-10 22:46:22 -0500

    Change Section for Unit and use JSON data

---
+++ src/components/Unit.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
+
+import {ProgressBar} from 'primereact/components/progressbar/ProgressBar';
+import {Button} from 'primereact/components/button/Button';
+
+import Material from './Material';
+
+class Unit extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      data: props.data,
+      index: props.index,
+      module: props.module,
+      progress: 50
+    }
+  }
+
+  onClickHandler = (e) => {
+    const { data, index, module } = this.state
+    ReactDOM.render(
+      <Material data={data} module={module} unit_index={index} />,
+      document.getElementById('material')
+    );
+  }
+
+  render() {
+    const { data, progress } = this.state
+    return (
+      <div className='ui-g'>
+        {data.description}
+        <div className='ui-g-8'>
+          <ProgressBar value={progress}></ProgressBar>
+        </div>
+        <div className='ui-g-4'>
+          <Button onClick={this.onClickHandler} label='Continuar'/>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Unit;
