4fdd3060d1c3e0ae7d0bb44aa9603cb1aaa393ac
Carlos A. Ospina <carlosaospinac@gmail.com>
2018-06-26 00:01:47 -0500

    Embedded activities in PDF included

--- src/components/ThematicContent.js
+++ src/components/ThematicContent.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
 
 import {Panel} from 'primereact/components/panel/Panel';
+import {Accordion,AccordionTab} from 'primereact/components/accordion/Accordion';
+
 
 
 class ThematicContent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      content: props.data.content,
+      activities: props.data.activities
+    }
+  }
+
+  componentWillReceiveProps = (props) => {
+    this.setState({
+      content: props.data.content,
+      activities: props.data.activities
+    })
+  }
+
   render(){
+    const { content, activities } = this.state;
+    console.log(content)
+    console.log(activities)
     return (
       <div>
         <Panel>
-          <h2>{this.props.data.content.title}</h2>
+          <h2>{content.title}</h2>
           {
-            this.props.data.content.body.map(function(val, index){
+            content.body.map(function(val, index){
               if (typeof(val) === 'string') {
                 return <p key={index}>{val}</p>
               } else {
@@ -27,8 +47,32 @@ class ThematicContent extends Component {
             })
           }
         </Panel>
+        {
+          activities.length > 0 &&
+          <div>
+            <h2>Actividades</h2>
+            {
+              activities.map((v, i) => {
+                return (
+                  <Accordion>
+                    <AccordionTab header={"Actividad".concat(i + 1)}>
+                    <div width="100%" height="315">
+                      <object data={require("./../data/activities/".concat(v, ".pdf"))}
+                      type="application/pdf" width="100%" height="315">
+                        <p>It appears you don't have a PDF plugin for this browser.
+                        No biggie... you can <a href={require("./../data/activities/".concat(v, ".pdf"))}>
+                        click here to
+                        download the PDF file.</a></p>  
+                      </object>
+                    </div>
+                    </AccordionTab>
+                  </Accordion>
+                )
+              })
+            }
+          </div>
+        }
       </div>
-
     )
   }
 }
